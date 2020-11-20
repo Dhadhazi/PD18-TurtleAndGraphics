2c6bbed21ce5d01a43ada9987f0b2eb225062a19
Dhadhazi <dhadhazi@gmail.com>
2020-11-20 11:06:10 +0100

    Random Walk

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import turtle as t
 import randomcolor
-
+import random
 
 timmy_the_turtle = t.Turtle()
+timmy_the_turtle.pensize(10)
+timmy_the_turtle.speed(0)
 rand_color = randomcolor.RandomColor()
 
-for i in range(4,9):
-    color = rand_color.generate()[0];
-    timmy_the_turtle.pencolor(color)
-    for _ in range(i):
-        timmy_the_turtle.forward(100)
-        timmy_the_turtle.right(360/i)
+def random_walk():
+    timmy_the_turtle.right(90*random.randint(0,4))
+    timmy_the_turtle.pencolor(rand_color.generate()[0])
+    timmy_the_turtle.forward(25)
+
+for i in range(150):
+    random_walk()
 
 screen = t.Screen()
 screen.exitonclick()
