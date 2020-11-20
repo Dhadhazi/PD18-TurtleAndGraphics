f51fefa70f6336d5cebf0eabdc2948b36c5eee74
Dhadhazi <dhadhazi@gmail.com>
2020-11-20 10:44:45 +0100

    Dashed line

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import turtle as t
 
 timmy_the_turtle = t.Turtle()
 
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(90)
+for _ in range(15):
+    timmy_the_turtle.pendown()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
 
 screen = t.Screen()
 screen.exitonclick()
