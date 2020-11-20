73b56a7e44f2538ed43e0f784173795a97f96909
Dhadhazi <dhadhazi@gmail.com>
2020-11-20 11:14:39 +0100

    Own color generator

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import turtle as t
-import randomcolor
+
 import random
 
+t.colormode(255)
 timmy_the_turtle = t.Turtle()
 timmy_the_turtle.pensize(10)
 timmy_the_turtle.speed(0)
-rand_color = randomcolor.RandomColor()
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return (r,g,b)
 
 def random_walk():
     timmy_the_turtle.right(90*random.randint(0,4))
-    timmy_the_turtle.pencolor(rand_color.generate()[0])
+    timmy_the_turtle.pencolor(random_color())
     timmy_the_turtle.forward(25)
 
 for i in range(150):
