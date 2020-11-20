7c4ff07e9b5ff1394aa9f576d1ae89664342f6b9
Dhadhazi <dhadhazi@gmail.com>
2020-11-20 10:59:05 +0100

    Shapes with random color

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import turtle as t
+import randomcolor
+
 
 timmy_the_turtle = t.Turtle()
+rand_color = randomcolor.RandomColor()
 
-for _ in range(15):
-    timmy_the_turtle.pendown()
-    timmy_the_turtle.forward(10)
-    timmy_the_turtle.penup()
-    timmy_the_turtle.forward(10)
+for i in range(4,9):
+    color = rand_color.generate()[0];
+    timmy_the_turtle.pencolor(color)
+    for _ in range(i):
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.right(360/i)
 
 screen = t.Screen()
 screen.exitonclick()
